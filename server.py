d9fa2b8d7bca8475a68cc75cf46afe8ef21388ae
Soul <35036294+Anyew@users.noreply.github.com>
2024-02-05 22:17:51 -0500

    To desktop

--- server.py
+++ server.py
@@ -4,19 +4,34 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
-from datetime import datetime
+from datetime import datetime    
  
 #Create Flask Instance
 app = Flask(__name__)
 
 #Add Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+#MySQL db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Supermariobros.3@localhost/sakila_test'
+
+
 
 #Secret Key
 app.config['SECRET_KEY'] = "password"
 
 #Initialized The Database
 db = SQLAlchemy(app)
+
+#In Context
+app.app_context().push()
+
+#Connect MySQL Database
+'''mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Supermariobros.3'
+app.config['MYSQL_DATABASE_DB'] = 'sakila'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)'''
 
 #Create Model
 class Users(db.Model):
@@ -29,16 +44,16 @@ class Users(db.Model):
     def __repr__(self):
         return '<Name %r>' % self.name 
 
-
+#Create Form Class
+class CustomerAddForm(FlaskForm):
+    name = StringField("Customer Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 #Create Form Class
 class CustomerNameForm(FlaskForm):
     name = StringField("Customer Name", validators=[DataRequired()])
     submit = SubmitField("Submit")
-
-#Create Database Within Context
-with app.app_context():
-    db.create_all()
 
 #Landing Page API route
 @app.route("/")
@@ -48,6 +63,25 @@ with app.app_context():
 
 def index():
     return render_template("index.html")
+
+#Add a Customer 
+@app.route("/customers/add", methods=['GET', 'POST'])
+
+def add_customer():
+    name = None
+    form = CustomerAddForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = Users(name=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data
+        form.name.data = ''
+        form.email.data = ''
+        flash("User Added Successfully")
+    our_users = Users.query.order_by(Users.date_added)
+    return render_template("add_customer.html", form=form, name=name, our_users=our_users) 
 
 #Move Search Page
 @app.route("/movies")
